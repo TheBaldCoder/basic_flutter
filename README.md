bc67facba1d841049ef3c708582375191fb54275
vanVulpenPaul <thebaldcoder@gmail.com>
2024-01-30 08:08:16 +0100

    Put a list of teams on the page.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # basic_flutter
 
-A basic Flutter project that will display data from a Laravel Api project.
+A basic Flutter project that will display data from the following API: https://www.balldontlie.io/api/v1/teams
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,52 @@
+import 'dart:convert';
+
+import 'package:basic_flutter/model/team.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<Team> teams = [];
+
+  Future getTeams() async {
+    var response = await http.get(Uri.http('balldontlie.io', 'api/v1/teams'));
+    var jsonData = jsonDecode(response.body);
+
+    for (var eachtTeam in jsonData['data']) {
+      final team = Team(
+          abbreviation: eachtTeam['abbreviation'], city: eachtTeam['city']);
+
+      teams.add(team);
+    }
+
+    print(teams.length);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+          future: getTeams(),
+          builder: (context, snapshot) {
+            // Is it done loading? Then show team data.
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                  itemCount: teams.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(teams[index].abbreviation),
+                      subtitle: Text(teams[index].city),
+                    );
+                  });
+            }
+            // Still loading, show Loading Circle
+            else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
   }
 }
